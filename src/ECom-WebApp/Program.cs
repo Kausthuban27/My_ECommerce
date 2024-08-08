61d5cad1dd568a0ed811d8c478142d6a9e231905
Kausthuban M <mkausthuban@ceiamerica.com>
2024-08-08 16:36:17 +0530

    Modified Bootstrap version

--- src/ECom-WebApp/Program.cs
+++ src/ECom-WebApp/Program.cs
@@ -1,11 +1,12 @@
 using ECom_WebApp.Components;
+using BlazorStrap;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddBlazorStrap();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
